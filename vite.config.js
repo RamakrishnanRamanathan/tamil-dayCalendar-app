5f87419ef76679ebec29d59a9a70137624900fc2
RamakrishnanRamanathan <ramakrishnanramanathan2003@gmail.com>
2024-05-19 12:13:42 +0530

    feat:debugging base path in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-   base: '/index.html'
+   base: '/tamil-day-calendar-app/'
   //   build: {
   //     rollupOptions: {
   //       input: {
