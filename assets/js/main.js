2a7e504d25e98be892acff9c40e7f915d1cb8439
RamakrishnanRamanathan <ramakrishnanramanathan2003@gmail.com>
2024-05-19 11:47:47 +0530

    feat:Add dynamic dates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-// Write your javascript code here
+const formDate = document.querySelector("#formDate");
+const imgContainer=document.querySelector('#dateimg')
+formDate.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const dateInput = document.querySelector("#dateInput").value;
+  imgContainer.classList.remove('hidden')
+  imgContainer.src=`https://www.prokerala.com/general/calendar/imgs/daily/${dateInput}.png`
+});
+
